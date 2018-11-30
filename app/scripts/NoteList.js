ff953e3597d3a2a578c7190f583eccd09f5a249d
Michel Momeyer <mjm87@students.calvin.edu>
2018-11-30 17:00:12 -0500

    Added boiler-plate react component code

---
+++ app/scripts/NoteList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import $ from 'jquery';
+
+// TODO: add other imports here
+import '../css/base.css';
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <div className="TODO:">
+        {/* Place JSX code here */}
+      </div>
+    );
+  }
+});
