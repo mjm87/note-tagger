de5df059187ff38aaadb298dd966b2a5cf17d43b
Michel Momeyer <mjm87@gold01.cs.calvin.edu>
2018-11-30 14:51:46 -0500

    Got a helloworld

--- app/scripts/NoteApp.js
+++ app/scripts/NoteApp.js
@@ -4,11 +4,11 @@ import $ from 'jquery';
 import '../css/base.css';
 
 module.exports = React.createClass({
-    render: function() {
-        return (
-            <div className ="NoteApp">
-                Helloworld!
-            </div>
-        );
-    }
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a Note-taker-appy-thing.
+      </div>
+    );
+  }
 });
