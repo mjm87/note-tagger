a2ed9ab12cb0952c2eea797a9df9662821b43a75
Michel Momeyer <mjm87@gold01.cs.calvin.edu>
2018-12-07 15:28:47 -0500

    misc. changes

--- app/scripts/EditableTagGroup.js
+++ app/scripts/EditableTagGroup.js
@@ -4,24 +4,29 @@ import $ from 'jquery';
 // TODO: add other imports here
 import '../css/base.css';
 import EditableTag from './EditableTag.js';
+import TagCreator from './TagCreator.js';
 
 module.exports = React.createClass({
 
+  addTag : function() {
+
+  },
   removeTag : function (){
-    Console.Log("TODO:");
+
   },
   render: function() {
 
-    var handleRemoval = this.removeTag;
+    var handleRemoval = this.removeTag;       // TODO: figure out what React-y nonsense is going on?
     var tagList = this.props.data.map(function(tag){
       return (
-       <EditableTag tagName={tag.name} handleClick={handleRemoval}/> 
+        <EditableTag tagName={tag.name} handleClick={handleRemoval}/> 
       );
     });
 
     return (
       <div className="EditableTagGroup">
         {tagList}
+        <TagCreator/>
       </div>
     );
   }
