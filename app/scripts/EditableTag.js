d24c9774cbdd6da25dada78075f3837c769ac5b5
Michel Momeyer <mjm87@students.calvin.edu>
2018-12-08 12:43:15 -0500

    Got external style sheets to work

--- app/scripts/EditableTag.js
+++ app/scripts/EditableTag.js
@@ -1,19 +1,12 @@
 import React from 'react';
 
-var editableTagStyle = {
-  background: 'aliceblue',
-  display: 'inline-block',
-  border: '1px solid black',
-  padding: '3px'  
-}
-
 module.exports = React.createClass({
   OnClick : function() {
     return this.props.handleClick(this.props.tagName);
   },
   render: function() {
     return (
-      <button type="button" onClick={this.OnClick} className="editableTag" style={editableTagStyle} >  
+      <button type="button" onClick={this.OnClick} className="editableTag" >  
           {this.props.tagName}
       </button>
     );
--- app/scripts/TagCreator.js
+++ app/scripts/TagCreator.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import '../css/base.css';
 
-// styling
-var tagCreatorStyle = {
-  background: 'aliceblue',
-  display: 'inline-block',
-  border: '1px solid black',
-  padding: '3px'  
-};
-
 module.exports = React.createClass({
 
     AddTag: function() {
@@ -16,7 +8,7 @@ module.exports = React.createClass({
     },
     render: function() {
         return (
-            <button className="TagCreator" onClick={this.AddTag} style={tagCreatorStyle}>
+            <button className="TagCreator" onClick={this.AddTag}>
                 +
             </button>
         );
