6172232f10d87de548e7de2373bb7516d14e3d71
Michel Momeyer <mjm87@students.calvin.edu>
2018-12-10 20:07:28 -0500

    Fixed the great disappearing content act of 2018

--- app/scripts/NoteContent.js
+++ app/scripts/NoteContent.js
@@ -9,13 +9,18 @@ module.exports = React.createClass({
   getInitialState: function() {
     return {text: ""};
   },
+  componentDidMount: function() {
+    this.setState({text: this.props.content});
+  },
   saveContent: function() {
     this.props.update(this.state.text);  
+    console.log("on blur ");
   },
   handleTextChanged: function(e) {
     this.setState({text: e.target.value});
   },
   render: function() {
+    console.log(this.props.content);
     return (
       <div className="CommentContent">
         <textarea
