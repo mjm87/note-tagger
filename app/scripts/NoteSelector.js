d7987210418b8ca07c2df946646763ecde32ba10
Michel Momeyer <mjm87@gold01.cs.calvin.edu>
2018-12-11 10:42:22 -0500

    misc debugging attempts

--- app/scripts/NoteSelector.js
+++ app/scripts/NoteSelector.js
@@ -9,10 +9,12 @@ module.exports = React.createClass({
     return ({ notes: [] , mounted: false});
   },
   componentDidMount() {
+    var tags = {tags: ["tag1", "tag3"]};
     $.ajax({
       url: "/filteredNotes",
       type: 'GET',
-      data: {tags: []},
+      data: tags,
+      contentType: 'application/json; charset=utf-8',
       dataType: 'json'
     })
       .done(function (results) {
